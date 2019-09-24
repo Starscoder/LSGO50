f465ea9a28813ecc5cad82407cad4ecb79ac7bcd
Starscoder <824048788@qq.com>
2019-09-24 16:12:34 +0800

    Create 32hasCycle.cpp

---
+++ 32hasCycle.cpp
@@ -0,0 +1,40 @@
+/**
+给定一个链表，判断链表中是否有环。
+
+为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+
+ 
+
+示例 1：
+
+输入：head = [3,2,0,-4], pos = 1
+输出：true
+解释：链表中有一个环，其尾部连接到第二个节点。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/linked-list-cycle
+**/
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* faptr = head;
+        ListNode* slptr = head;
+        while(faptr != NULL && faptr->next != NULL){
+            faptr = faptr->next->next;
+            slptr = slptr->next;
+            if(faptr == slptr)
+                return true;
+        }
+        return false;
+    }
+};
